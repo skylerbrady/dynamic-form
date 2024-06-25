240071bd38cb74c2584d00ae0a4b10828d5129cf
Akash Hegde <akash.hegde@accionlabs.com>
2024-06-25 15:15:14 +0530

    Added dynamic table

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -1,44 +1,62 @@
 import React, { useState } from "react";
-import "./DynamicForm.css"; // Import CSS file for styling
+import Table from "./Table";
+import "./DynamicForm.css";
 
 const DynamicForm = ({ questionnaire }) => {
-  const [answers, setAnswers] = useState({});
-
-  const handleChange = (questionId, answer) => {
-    setAnswers({ ...answers, [questionId]: answer });
+  const [activeTab, setActiveTab] = useState(0);
+  const handleTabClick = (index) => {
+    setActiveTab(index);
   };
-
-  return (
-    <div className="dynamic-form">
-      {questionnaire.Section.Question.map((question) => (
-        <div className="question" key={question.$.ID}>
-          <p>{question["QuestionText"]}</p>
-          {question["AnswerType"] === "Radio Button" && (
-            <div className="answer-options">
-              {question["AnswerData"].split(",").map((option) => (
-                <label key={option.trim()}>
-                  <input
-                    type="radio"
-                    value={option.trim()}
-                    checked={answers[question.$.ID] === option.trim()}
-                    onChange={() => handleChange(question.$.ID, option.trim())}
-                  />
-                  {option.trim()}
-                </label>
-              ))}
-            </div>
-          )}
-          {question["AnswerType"] === "Text" && (
-            <input
-              className="text-input"
-              type="text"
-              value={answers[question.$.ID] || ""}
-              onChange={(e) => handleChange(question.$.ID, e.target.value)}
-            />
-          )}
+  const Question = ({ question }) => (
+    <div className="question" key={question.$.ID}>
+      <p>{question["QuestionText"]}</p>
+      {question["AnswerType"] === "Radio Button" && (
+        <div className="answer-options">
+          {question["AnswerData"].split(",").map((option) => (
+            <label key={option.trim()}>
+              <input type="radio" />
+              {option.trim()}
+            </label>
+          ))}
         </div>
-      ))}
+      )}
+      {question["AnswerType"] === "Text" && (
+        <input className="text-input" type="text" />
+      )}
+      {question["AnswerType"] === "Table" && <Table tableData={question} />}
     </div>
+  );
+  return (
+    <>
+      <div className="tab-headers">
+        {questionnaire.Section.map((section, index) => (
+          <button
+            key={index}
+            onClick={() => handleTabClick(index)}
+            className={activeTab === index ? "active" : ""}
+          >
+            {section.$.title}
+          </button>
+        ))}
+      </div>
+
+      <form className="dynamic-form">
+        {questionnaire.Section.map((section, sectionIndex) => (
+          <div
+            key={sectionIndex}
+            style={{ display: activeTab === sectionIndex ? "block" : "none" }}
+          >
+            {section.Question.length > 0 ? (
+              section.Question.map((question) => (
+                <Question question={question} />
+              ))
+            ) : (
+              <Question question={section.Question} />
+            )}
+          </div>
+        ))}
+      </form>
+    </>
   );
 };
 
---
+++ src/Table.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./table.css";
+const Table = ({ tableData }) => {
+  const { TableRow, TableData } = tableData;
+  const headers = TableRow.split(",").map((header) => header.trim());
+  const rows = [];
+  let currentRow = [];
+  TableData.forEach((data, index) => {
+    const { TableDataType, TableDataValue } = data;
+    currentRow.push({ type: TableDataType, value: TableDataValue });
+    if ((index + 1) % headers.length === 0) {
+      rows.push(currentRow);
+      currentRow = [];
+    }
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, cellIndex) => (
+              <td key={cellIndex}>
+                {cell.type === "text" && cell.value}
+                {cell.type === "dropdown" && (
+                  <select>
+                    {cell.value.split(",").map((option, optionIndex) => (
+                      <option key={optionIndex} value={option.trim()}>
+                        {option.trim()}
+                      </option>
+                    ))}
+                  </select>
+                )}
+                {cell.type === "TextField" && <input type="text" />}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+export default Table;
